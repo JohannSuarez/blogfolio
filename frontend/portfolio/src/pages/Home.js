617451488b8366856c73090cde7efdcad028c538
Johann Suarez <johann.suarez92@gmail.com>
2022-12-21 01:47:31 -0800

    More styling to Home

--- frontend/portfolio/src/pages/Home.js
+++ frontend/portfolio/src/pages/Home.js
@@ -8,8 +8,15 @@ function HomeComponents() {
     <div className="home-components">
       <div className="home-info">
         <h2 className="job-title">Junior Fullstack Developer</h2>
-        <h1>Hello, my name is Johann Suarez</h1>
-        <h2>And yet the menace of the years finds and shall find me unafraid.</h2>
+        <h1 className="greeting">Hello, my name is Johann Suarez</h1>
+        <h2 className="description">
+          I'm a 3rd Year Computer Science student with
+          industry experience as a backend developer.
+          Short text with details about you,
+          what you do or your professional career.
+          You can add more information on the about page.
+        </h2>
+
         <div className="button-group">
           <h4>button_1</h4>
           <h4>button_2</h4>
