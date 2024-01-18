e4c9bbb29c76970b13d4927d4063affc03fa83b4
Johann Suarez <johann.suarez92@gmail.com>
2024-01-17 22:49:57 -0800

    Added SKetchfab embed

--- src/pages/Projects.js
+++ src/pages/Projects.js
@@ -1,9 +1,52 @@
-import UnderConstruction from '../components/UnderConstruction.js'
+// Remove this import if you're not using the UnderConstruction component
+// import UnderConstruction from '../components/UnderConstruction.js'
 
 export default function Projects() {
+  return <ProjectsComponents />;
+}
+
+function ProjectsComponents() {
   return (
     <div>
-      <UnderConstruction/>
+      <div className="sketchfab-embed-wrapper">
+        <iframe
+          title="pH Reading System Case and Mount"
+          frameBorder="0"
+          allowFullScreen
+          mozallowfullscreen="true"
+          webkitallowfullscreen="true"
+          allow="autoplay; fullscreen; xr-spatial-tracking"
+          xr-spatial-tracking
+          execution-while-out-of-viewport
+          execution-while-not-rendered
+          web-share
+          src="https://sketchfab.com/models/8ee734ea2b724d068e0283ce7d61ca3d/embed?autospin=1&autostart=1"
+        ></iframe>
+        <p style={{ fontSize: "13px", fontWeight: "normal", margin: "5px", color: "#4A4A4A" }}>
+          <a
+            href="https://sketchfab.com/3d-models/ph-reading-system-case-and-mount-8ee734ea2b724d068e0283ce7d61ca3d?utm_medium=embed&utm_campaign=share-popup&utm_content=8ee734ea2b724d068e0283ce7d61ca3d"
+            target="_blank"
+            rel="noopener noreferrer"
+            style={{ fontWeight: "bold", color: "#1CAAD9" }}
+          >
+            pH Reading System Case and Mount
+          </a> by <a
+            href="https://sketchfab.com/navier_stoked?utm_medium=embed&utm_campaign=share-popup&utm_content=8ee734ea2b724d068e0283ce7d61ca3d"
+            target="_blank"
+            rel="noopener noreferrer"
+            style={{ fontWeight: "bold", color: "#1CAAD9" }}
+          >
+            wmosci
+          </a> on <a
+            href="https://sketchfab.com?utm_medium=embed&utm_campaign=share-popup&utm_content=8ee734ea2b724d068e0283ce7d61ca3d"
+            target="_blank"
+            rel="noopener noreferrer"
+            style={{ fontWeight: "bold", color: "#1CAAD9" }}
+          >
+            Sketchfab
+          </a>
+        </p>
+      </div>
     </div>
-  )
+  );
 }
