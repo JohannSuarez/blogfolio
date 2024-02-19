5d75de77c134b7a40d73a8634c01d5abc0bf2063
Johann Suarez <johann.suarez92@gmail.com>
2024-02-18 23:46:25 -0800

    pH sensor post complete

--- src/pages/Projects.js
+++ src/pages/Projects.js
@@ -42,7 +42,7 @@ function ProjectsComponents() {
                 The solution I came up with was to build a Python program that launched <a href="https://dash.plotly.com/">Plotly Dash</a>, and then ran two threads: The first thread reads incoming data from the Arduino
                 from the serial port. The data picked up by the first thread is copied onto a shared queue. The second thread's job is to periodically
                 check if the shared queue has new data. If the second thread notices there is new data, it then parses it into JSON data that Plotly Dash
-                components can recognize. The Plotly Dash is then refreshed and then the end result is a webpage with components that graphed realtime sensor data.
+                components can recognize. The Plotly Dash components are then refreshed and then the end result is a webpage with components that graphed realtime sensor data.
 
               </p>
 
@@ -81,93 +81,103 @@ function ProjectsComponents() {
 
 
 
-            <p>These limitations were beyond the scope of the contract I finished. However, I took a crack at addressing them
-            in my spare time. The result of my brainstorming was a pH reading system powered by an ESP32 that had a calibration mode.
-            The component list is as follows:
+              <p class="clearfix sole_paragraph">These limitations were beyond the scope of the contract I finished. However, I took a crack at addressing them
+              in my spare time. The result of my brainstorming was a pH reading system powered by an ESP32 that had a calibration mode.
+              The component list is as follows:
 
-            <ul>
-              <li>ESP32 Microcontroller - For reading the pH sensor's values, storing calibration offsets, and sending the reading back to the Raspberry Pi through WiFi.</li>
-              <li>4x4 Matrix Membrane Keypad - For going into calibration mode, an ESP32 thread waited for a sequence of key presses (ex: "C" then "A")</li>
-              <li>pH-4052 pH sensor</li>
-              <li>128 x 64 0.91" OLED Display - Regularly displays the pH reading, warns of network issues, and also guides user through calibration mode.</li>
-              <li>3D Printed PLA Case - I needed a housing to hold all of the components together.</li>
+              <ul>
+                <li><b>ESP32</b> - An inexpensive yet powerful microcontroller, used for receiving the pH sensor's values, storing calibration offsets, and sending the pH values back to the Raspberry Pi through WiFi.</li>
+                <li><b>4x4 Matrix Membrane Keypad</b> - For going into calibration mode, an ESP32 thread waited for a sequence of key presses (ex: "C" then "A")</li>
+                <li><b>pH-4052 Module</b> - Module for measuring pH levels. It receives an analog voltage from a pH probe and converts it into readable pH values. </li>
+                <li><b>128 x 64 0.91" OLED Display</b> - Regularly displays the pH reading, warns of network issues, and also guides user through calibration mode.</li>
+                <li><b>3D Printed PLA Case</b> - I 3D modelled a housing to hold all of the components together.</li>
 
-            </ul>
-            </p>
+              </ul>
+              </p>
 
-            <div className="sketchfab-embed-wrapper">
-              <iframe
-                title="pH Reading System Case and Mount"
-                allowFullScreen
-                mozallowfullscreen="true"
-                webkitallowfullscreen="true"
-                allow="autoplay; fullscreen; xr-spatial-tracking"
-                xr-spatial-tracking
-                execution-while-out-of-viewport
-                execution-while-not-rendered
-                web-share
-                src="https://sketchfab.com/models/8ee734ea2b724d068e0283ce7d61ca3d/embed?autospin=1&autostart=1"
-              ></iframe>
-                <p style={{ fontSize: "13px", fontWeight: "normal", margin: "5px", color: "#4A4A4A" }}>
-                  <a
-                    href="https://sketchfab.com/3d-models/ph-reading-system-case-and-mount-8ee734ea2b724d068e0283ce7d61ca3d?utm_medium=embed&utm_campaign=share-popup&utm_content=8ee734ea2b724d068e0283ce7d61ca3d"
-                    target="_blank"
-                    rel="noopener noreferrer"
-                    style={{ fontWeight: "bold", color: "#1CAAD9" }}
-                  >
-                    pH Reading System Case and Mount
-                  </a> by <a
-                    href="https://sketchfab.com/navier_stoked?utm_medium=embed&utm_campaign=share-popup&utm_content=8ee734ea2b724d068e0283ce7d61ca3d"
-                    target="_blank"
-                    rel="noopener noreferrer"
-                    style={{ fontWeight: "bold", color: "#1CAAD9" }}
-                  >
-                    wmosci
-                  </a> on <a
-                    href="https://sketchfab.com?utm_medium=embed&utm_campaign=share-popup&utm_content=8ee734ea2b724d068e0283ce7d61ca3d"
-                    target="_blank"
-                    rel="noopener noreferrer"
-                    style={{ fontWeight: "bold", color: "#1CAAD9" }}
-                  >
-                    Sketchfab
-                  </a>
-                </p>
+            <div class="row_set">
+              <div className="sketchfab-embed-wrapper">
+                <iframe
+                  title="pH Reading System Case and Mount"
+                  allowFullScreen
+                  mozallowfullscreen="true"
+                  webkitallowfullscreen="true"
+                  allow="autoplay; fullscreen; xr-spatial-tracking"
+                  xr-spatial-tracking
+                  execution-while-out-of-viewport
+                  execution-while-not-rendered
+                  web-share
+                  width="640" 
+                  height="480" 
+                  src="https://sketchfab.com/models/8ee734ea2b724d068e0283ce7d61ca3d/embed?autospin=1&autostart=1"
+                ></iframe>
+                  <p style={{ fontSize: "13px", fontWeight: "normal", margin: "5px", color: "#4A4A4A" }}>
+                    <a
+                      href="https://sketchfab.com/3d-models/ph-reading-system-case-and-mount-8ee734ea2b724d068e0283ce7d61ca3d?utm_medium=embed&utm_campaign=share-popup&utm_content=8ee734ea2b724d068e0283ce7d61ca3d"
+                      target="_blank"
+                      rel="noopener noreferrer"
+                      style={{ fontWeight: "bold", color: "#1CAAD9" }}
+                    >
+                      pH Reading System Case and Mount
+                    </a> by <a
+                      href="https://sketchfab.com/navier_stoked?utm_medium=embed&utm_campaign=share-popup&utm_content=8ee734ea2b724d068e0283ce7d61ca3d"
+                      target="_blank"
+                      rel="noopener noreferrer"
+                      style={{ fontWeight: "bold", color: "#1CAAD9" }}
+                    >
+                      wmosci
+                    </a> on <a
+                      href="https://sketchfab.com?utm_medium=embed&utm_campaign=share-popup&utm_content=8ee734ea2b724d068e0283ce7d61ca3d"
+                      target="_blank"
+                      rel="noopener noreferrer"
+                      style={{ fontWeight: "bold", color: "#1CAAD9" }}
+                    >
+                      Sketchfab
+                    </a>
+                  </p>
+              </div>
             </div>
 
-            <img alt="completed pH sensor kit setup" className="irl_ph_sensor"
-              src='https://i.imgur.com/KwyLlw4.jpg'
-              width="350px"
-            />
-            <img alt="close-up of oled display on pH sensor kit" className="irl_ph_sensor_oled"
-              src='https://i.imgur.com/uzMdvOr.jpg'
-              width="350px"
-            />
-            <img alt="the sensor kit case dimensions on Blender" className="ph_sensor_dimensions"
-              src='https://i.imgur.com/Qczu3Tg.png'
-              width="350px"
-            />
-            <img alt="3D printer base case of sensor kit" className="ph_sensor_empty_case"
-              src='https://i.imgur.com/L7AdSkD.png'
-              width="350px"
-            />
-            <img alt="component diagram of sensor kit" className="ph_sensor_component_diagram"
-              src='https://i.imgur.com/7014Xl0.png'
-              width="350px"
-            />
+            <p class="clearfix sole_paragraph">
+            The planned implementation had one of these units mounted above each water tank.
 
-              
+            The WiFi communication offered by the ESP32 meant voltage interference from having long physical
+            wires were no longer an issue. With an HTTP server running locally on each Raspberry Pi, each ESP32
+            unit can communicate via HTTP POST to their designated Arduino to report their current pH reading.
+
+            If a unit needed its pH sensor probe's weekly calibration, one simply keys in "C" and "A". The OLED display then instructs the 
+            user to dip the pH probe in two diffent pH levels, and afterwards automatically calculates the offset, and saves the offset
+            in persistent memory.
+
+            </p>
+
+            <div class="row_set">
+              <figure class="cap-bot">
+                <img alt="completed pH sensor kit setup" width="855" className="irl_ph_sensor"
+                  src='https://i.imgur.com/KwyLlw4.jpg'
+                />
+                <figcaption>
+                  Actual implementation. At the time I had no 128x64 OLED, so I used a 128x32
+                </figcaption>
+              </figure>
+
+              <figure class="cap-bot">
+                <img alt="planned_esp32_implementation" src="https://i.imgur.com/eWHdzNH.jpg"/>
+                <figcaption>
+                  The pH reading and calibration is modularized and physically separated from the Arduino.
+                </figcaption>
+              </figure>
+
+            </div>
 
 
+            <p class="clearfix sole_paragraph">
+              A great feature of my pH reading unit is it allowed anyone to view the current pH reading of an algae tank without
+              needing to walk back to the desktop monitor. I designed it to function within the lab's system and also completely independently.
+              Relying on a ESP32 board, it can be powered by just a 5v power bank. Lastly, it will display the pH data through the OLED even before
+              having successfully connected to the algae farm's local area network.
+            </p>
 
-              <br/>
-              Summary:  Maintained and improved algae generation tank microcontroller systems.
-              Each tank is monitored by Arduino Megas hooked with sensors to collect light, temperature,
-              and pH levels. The microcontroller uses relay switches to adjust these parameters for algae growth.
-
-              <br/>
-              Contributions:
-              Wrote a program to read from Arduino's serial connection port, parsed the data, and then converted
-              it into tabular data for real-time plotting and visualization using Plotly Dash.
           </section>
         </article>
       </BlogPostCard>
@@ -182,32 +192,58 @@ function ProjectsComponents() {
       </BlogPostCard>
       */}
 
-
-      <BlogPostCard className="project1">
-        <h1>ChromaBlend: A Fix to DeepRemaster's Low Resolution Limitation</h1>
-        <div className="chromablend">
-          <iframe width="560" height="315" src="https://www.youtube.com/embed/IBmc5fa9X9A?si=E7mDBSW0vxQz8xPO" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
-        </div>
-
-        <br/>
-        <img alt="animation displaying chroma blend's function" className="chroma_blend_animation"
-          src='https://i.imgur.com/3ffp1Fz.gif'
-          width="350px"
-        />
-
-
-        <br/>
-        <img alt="handpainted reference frame" className="reference_frame_manual_colorization"
-          src='https://i.imgur.com/ZuVE3go.jpg'
-          width="350px"
-        />
-
+      <BlogPostCard className="face_tracking_webcam">
+        <article>
+          <header>
+            <h1>Face Tracking With a Webcam and Two Servo Motors</h1>
+            <p>By <span class="author">Johann Suarez</span> on <time datetime="YYYY-MM-DD">August 20, 2020</time></p> 
+          </header>
+          <h3>No need for computationally heavy AI, the Viola-Jones algorithm can do the job.</h3>
+          <h3>Under Construction. Stay tuned! </h3>
+          <iframe width="560" height="315" src="https://www.youtube.com/embed/TcmhbfcM2XQ?si=GolZ1qMyJUD9ZZ0y" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
+        </article>
       </BlogPostCard>
 
-  <BlogPostCard className="face_tracking_webcam">
-    <h1>Face Tracking With a Webcam and Two Servo Motors</h1>
-    <iframe width="560" height="315" src="https://www.youtube.com/embed/TcmhbfcM2XQ?si=GolZ1qMyJUD9ZZ0y" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
-  </BlogPostCard>
+      <BlogPostCard className="project1">
+        <article>
+
+          <header>
+            <h1>ChromaBlend: A Fix to DeepRemaster's Low Resolution Limitation</h1>
+            <p>By <span class="author">Johann Suarez</span> on <time datetime="YYYY-MM-DD">May 21, 2020</time></p> 
+          </header>
+
+          <section>
+            <p>
+              <h3>For the purpose of colorizing black and white films without painstakingly handpainting each frame.</h3>
+              <h3>Under Construction. Stay tuned! </h3>
+              <h3>In the meantime, see: <a href="https://github.com/JohannSuarez/chroma_blend">ChromaBlend on Github</a></h3>
+            </p>
+            <div class="row_set">
+              <div className="chromablend">
+                <iframe width="560" height="315" src="https://www.youtube.com/embed/IBmc5fa9X9A?si=E7mDBSW0vxQz8xPO" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
+              </div>
+
+            </div>
+
+            <div class="row_set">
+                <img alt="animation displaying chroma blend's function" className="chroma_blend_animation"
+                  src='https://i.imgur.com/3ffp1Fz.gif'
+                  width="560px"
+                />
+            </div>
+
+            <div class="row_set">
+              <img alt="handpainted reference frame" className="reference_frame_manual_colorization"
+                src='https://i.imgur.com/ZuVE3go.jpg'
+                width="350px"
+              />
+            </div>
+
+            <br/>
+          </section>
+        </article>
+      </BlogPostCard>
+
     </div>
   );
 }
